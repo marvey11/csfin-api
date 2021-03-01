0b8c47ee7dd78f3bc7a88633867d47a67f024591
Marco Wegner <devel@marcowegner.eu>
2021-03-01 21:05:55 +0100

    Issue #6 -- Insert-or-update-quotes works

--- src/controllers/quote-controller.ts
+++ src/controllers/quote-controller.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import { Body, JsonController, Post, Res } from "routing-controllers";
 import { Service } from "typedi";
 import { AddQuoteDataRequest } from "../dtos";
@@ -10,11 +11,14 @@ class QuoteDataController {
     constructor(private service: QuoteDataService) {}
 
     @Post("/quotes")
-    async add(@Body({required: true}) data: AddQuoteDataRequest, @Res() response: Response): Promise<Response> {
-        console.log(data);
-        await this.service.add(data);
-        return response.send(data);
+    async add(@Body({ required: true }) data: AddQuoteDataRequest, @Res() response: Response): Promise<Response> {
+        try {
+            await this.service.add(data);
+            return response.status(StatusCodes.NO_CONTENT).send();
+        } catch (error) {
+            return response.status(StatusCodes.BAD_REQUEST).send({ message: error.message });
+        }
     }
 }
 
-export {QuoteDataController};
+export { QuoteDataController };
