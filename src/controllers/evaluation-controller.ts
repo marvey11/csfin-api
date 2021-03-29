d3b2d11192c5206f904a25e90c9d84467ebd3087
Marco Wegner <673439+marvey11@users.noreply.github.com>
2021-03-29 17:20:47 +0200

    Working on improving the RSL evaluation
    - implemented a new RSL method where the JOINs to the securities and
      exchanges tables only happen in the outer query instead of in the
      inner
    - also moved all the RSL evaluation functionality out the the
      QuoteDataService and into its own service class (there's enough code
      there that ´this is justified, and there's also litte to no chance
      that any of the RSL code might actually be re-used somewhere else)
    - will probably do the same for the performance evakluation...
    - with the data I have (25,000 to 30,000 price entries) I am not seeing
      a real difference in performance (calling this in ARC takes 150 to
      250 ms) with each of the implementation
    - since I think the new implementation is cleaner it stays

--- src/controllers/evaluation-controller.ts
+++ src/controllers/evaluation-controller.ts
@@ -2,14 +2,14 @@ import { Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { Get, JsonController, QueryParam, Res } from "routing-controllers";
 import { Service } from "typedi";
-
+import { RSLevyResponseData, RSLevyService } from "../services";
 import { EvaluationService } from "../services/evaluation-service";
 import { PerformanceIntervalDTO } from "../services/quote-service";
 
 @Service()
 @JsonController()
 class EvaluationController {
-    constructor(private service: EvaluationService) {}
+    constructor(private service: EvaluationService, private rslService: RSLevyService) {}
 
     @Get("/evaluate/performance")
     async evaluatePerformance(
@@ -22,7 +22,7 @@ class EvaluationController {
 
     @Get("/evaluate/rsl-data")
     async getRSLevyData(@Res() response: Response): Promise<Response> {
-        const data = await this.service.getRSLevyData();
+        const data: RSLevyResponseData[] = await this.rslService.getRSLevyData();
         return response.status(StatusCodes.OK).send(data);
     }
 }
